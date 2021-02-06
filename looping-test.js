89fa4110041f52bf1579b893eb34e9cb7430b3e5
Dennis Wong <denniswong0106@gmail.com>
2021-02-06 08:44:51 +0000

    here I added the a file where I messed around with Loops, basic stuff

---
+++ looping-test.js
@@ -0,0 +1,14 @@
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  if(repeat % 2) {
+    console.log("Let's move!");
+  } else {
+  console.log(chorus);
+  }
+  repeat++;
+  
+}
+
+console.log("Until the sun comes up!");
+
