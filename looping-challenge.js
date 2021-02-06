bcb19b2e056f10ebc836b5722d4d951dab6f5271
Dennis Wong <denniswong0106@gmail.com>
2021-02-06 10:02:26 +0000

    my code for a loop that iterates from 100 to 200, where numbers divisible by 3 are ouputted 'loopy', divisible by 4 is outputted 'lighthouse' and divisible by 3 and 4 are outputted by 'loopylighthouse'

---
+++ looping-challenge.js
@@ -0,0 +1,11 @@
+for (i = 100; i <= 200; i++) {
+  if (!(i % 3) && !(i % 4)) {
+    console.log('LoopyLighthouse');
+  } else if (!(i % 4)) {
+    console.log('Lighthouse');
+  } else if (!(i % 3)) {
+    console.log('Loopy');
+  } else {
+    console.log(i);
+  }
+}
